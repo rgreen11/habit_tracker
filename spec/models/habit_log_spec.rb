adc74b29b426f8d7f42e88d2bc5e5a97cd5b5c30
Rich G <richg@richs-mbp.mynetworksettings.com>
2025-07-20 22:44:14 -0400

    added more habit log spec

--- spec/models/habit_log_spec.rb
+++ spec/models/habit_log_spec.rb
@@ -1,6 +1,14 @@
 require "rails_helper"
 
 RSpec.describe HabitLog, type: :model do
+  let!(:user)  { User.create!(email: "test@example.com", password: "password") }
+  let!(:habit) { user.habits.create!(name: "Exercise", frequency_unit: "day", frequency_value: 1, start_date: Date.today) }
+
+  it "is invalid without a status" do
+    habit_log = HabitLog.new(status: 'pending', habit_id: habit.id)
+    expect(habit_log).to be_valid
+  end
+
   it "is invalid without a status" do
     habit_log = HabitLog.new(status: nil)
     expect(habit_log).to_not be_valid
